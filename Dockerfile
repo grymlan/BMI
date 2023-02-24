2441e9cb87c177071ae4bfe288228d54900137bd
Casey Lyon <grymlan@gmail.com>
2023-02-24 15:39:56 -0600

    added error handling, dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ ENV APP_HOME /app
 WORKDIR $APP_HOME
 COPY . ./
 
-RUN pip install -r requirements.txt
+RUN python3 -m pip install -r requirements.txt
 
 CMD exec gunicorn --bind :$PORT --workers 1 --worker-class uvicorn.workers.UvicornWorker  --threads 8 bmi:app
--- bmi.py
+++ bmi.py
@@ -17,16 +17,19 @@ async def get_percentile(gender: str, age: float, bmi: float):
         Specify 'male' or 'female'
     
     '''
-    if gender == 'male':
-        closest_age_percentiles = df_male.loc[(df_male['Age (in months)']-age).abs().idxmin()]
-        diffs = abs(closest_age_percentiles- bmi)
-        closest_col = diffs.idxmin()
-        percentile = closest_col.split()[0]
-        return {'percentile': f'{percentile} percentile' }
-    if gender == 'female':
-        closest_age_percentiles = df_female.loc[(df_female['Age (in months)']-age).abs().idxmin()]
-        diffs = abs(closest_age_percentiles- bmi)
-        closest_col = diffs.idxmin()
-        percentile = closest_col.split()[0]
-        return {'percentile': f'{percentile} percentile' }
+    try:
+        if gender in ['male','Male','M','m']:
+            closest_age_percentiles = df_male.loc[(df_male['Age (in months)']-age).abs().idxmin()]
+            diffs = abs(closest_age_percentiles- bmi)
+            closest_col = diffs.idxmin()
+            percentile = closest_col.split()[0]
+            return {'percentile': f'{percentile} percentile' }
+        if gender in ['female','Female','F','f']:
+            closest_age_percentiles = df_female.loc[(df_female['Age (in months)']-age).abs().idxmin()]
+            diffs = abs(closest_age_percentiles- bmi)
+            closest_col = diffs.idxmin()
+            percentile = closest_col.split()[0]
+            return {'percentile': f'{percentile} percentile' }
+    except Exception as e:
+        pass
     
